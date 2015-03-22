eda9e6f465962784d206d5d3855eb7d8d6a05cf9
James McKay <dcseee-github@yahoo.com.au>
2015-03-23 10:22:12 +1100

    Set executable name

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ import sys
 from cx_Freeze import setup, Executable
 
 base = None
+targetName = "cursePackDownloader"
 if sys.platform == "win32":
     base = "Win32GUI"
+    targetName = "cursePackDownloader.exe"
 
 setup(
     name = "cursePackDownloader",
     version = "0.1",
     description = "Download extra mods from Curse-hosted Minecraft modpacks",
-    executables = [Executable("downloader.py")])
+    executables = [Executable("downloader.py", targetName=targetName)])

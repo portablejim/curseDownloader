1daf2b0506a31489999367c26091b54632439588
portablejim <dcseee-github@yahoo.com.au>
2017-09-23 14:20:17 +1000

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@
 
 import sys
 from cx_Freeze import setup, Executable
-
-# <added>
 import os.path
+
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
-# </added>
-
-#  appdirs,idna,chardet,urllib3,certifi,requests
-build_exe_options = {"packages": ["appdirs","idna","chardet","urllib3","certifi","requests"],"include_files": ["tcl86t.dll", "tk86t.dll"]}
 
 base = None
 targetName = "cursePackDownloader"
@@ -20,7 +15,6 @@ if sys.platform == "win32":
     targetName = "cursePackDownloader.exe"
 
 
-# <added>
 options = {
     'build_exe': {
         'include_files':[
@@ -29,7 +23,6 @@ options = {
          ],
     },
 }
-# </added>
 
 setup(
     name="cursePackDownloader",
